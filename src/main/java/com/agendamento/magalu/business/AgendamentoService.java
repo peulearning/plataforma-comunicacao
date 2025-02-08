545508cef36deb0ceac3e53a75c56924b6fb834c
Pedro <peuhenry2018@gmail.com>
2025-02-08 09:32:02 -0300

    Refactoring Agendamento Service

--- src/main/java/com/agendamento/magalu/business/AgendamentoService.java
+++ src/main/java/com/agendamento/magalu/business/AgendamentoService.java
@@ -1,5 +1,8 @@
 package com.agendamento.magalu.business;
 
+import com.agendamento.magalu.business.mapper.IAgendamentoMapper;
+import com.agendamento.magalu.infra.entities.Agendamento;
+import com.agendamento.magalu.infra.repositories.AgendamentoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +10,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class AgendamentoService {
 
+    private  AgendamentoRepository repository;
+    private  IAgendamentoMapper agendamentoMapper;
+
+    public Agendamento gravarAgendamento(AgendamentoService agendamento){
+        return agendamentoMapper.paraOut(
+                repository.save(
+                        agendamentoMapper.paraEntity(agendamento)));
+    }
 }
