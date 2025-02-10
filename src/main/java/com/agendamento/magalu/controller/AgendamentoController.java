e3b5f4606ae80ac7398f9c34cad19684b4b7316d
Pedro <peuhenry2018@gmail.com>
2025-02-10 11:51:22 -0300

    EndPoint Cancelar Agendamento

--- src/main/java/com/agendamento/magalu/controller/AgendamentoController.java
+++ src/main/java/com/agendamento/magalu/controller/AgendamentoController.java
@@ -22,4 +22,10 @@ public class AgendamentoController {
     public ResponseEntity<Agendamento> buscarAgendamentoPorId(@PathVariable("id") Long id){
         return ResponseEntity.ok(agendamentoService.buscarAgendamentosPorId(id));
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> cancelarAgendamento(@PathVariable("id") Long id){
+        agendamentoService.cancelarAgendamento(id);
+        return ResponseEntity.accepted().build();
+    }
 }
